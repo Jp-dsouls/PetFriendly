deb8a9acaa2286b21bedd0e47774c9c6b08e6452
Marin Espinoza Ramirez <mespinozar1090@gmail.com>
2022-07-07 23:04:26 -0500

    marin

--- app/src/main/java/com/utp/petfriendly/model/TiendaModel.java
+++ app/src/main/java/com/utp/petfriendly/model/TiendaModel.java
@@ -1,2 +1,42 @@
-package com.utp.petfriendly.model;public class TiendaModel {
+package com.utp.petfriendly.model;
+
+import java.io.Serializable;
+
+public class TiendaModel implements Serializable {
+    String nombreProducto;
+    Double precio;
+    Integer cantidad;
+    String imagen;
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
 }
